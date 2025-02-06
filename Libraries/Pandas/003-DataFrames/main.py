c7b64ee3541f5d917cb87175971bff227cf27d3c
Daniel Yoshizawa <yoshidanielcwb@gmail.com>
2025-02-06 09:35:23 -0300

    Pandas - DataFrame

---
+++ Libraries/Pandas/003-DataFrames/main.py
@@ -0,0 +1,71 @@
+# DataFrames
+
+# A Pandas DataFrame is a 2 dimensional data structure, like a 2 dimensional array, or a table with rows and columns.
+
+# Example
+
+import pandas as pd
+
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+}
+
+# load data into a DataFrame object:
+
+df = pd.DataFrame(data)
+
+print(df)
+
+# Locate Row
+
+# As you can see from the result of print(df), the DataFrame is like a table with rows and columns.
+
+# Pandas use the loc attribute to return one or more specified row(s)
+
+# Example 
+
+# Return row 0
+
+print(df.loc[0]) # refer to the row index
+
+# NOTE : This example returns a Pandas Series.
+
+# Example 
+
+# Return row 0 and 1
+
+print(df.loc[[0, 1]]) # use a list of indexes
+
+# NOTE : When using [], the result is a Pandas DataFrame.
+
+# Named Indexes
+
+# With the index argument, you can name your own indexes.
+
+# Example
+
+# Add a list of names to give each row a name:
+
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df)
+
+# Locate Named Indexes
+
+# Use the named index in the loc attribute to return the specified row(s).
+
+# Example
+
+print(df.loc["day2"]) # refer to the named index
+
+# Load files intoo a DataFrame
+
+df = pd.read_csv('data.csv')
+
+print(df)
